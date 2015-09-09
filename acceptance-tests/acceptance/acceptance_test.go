e0689f8f150459c5360d9c977c7f35e9b137a27b
Ryan Faerman <ryan_faerman@ultimatesoftware.com>
2015-09-08 23:50:17 -0400

    Add acceptance test runner

---
+++ acceptance-tests/acceptance/acceptance_test.go
@@ -0,0 +1,175 @@
+package main_test
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"testing"
+	"time"
+
+	"camlistore.org/pkg/netutil"
+
+	"github.com/franela/goreq"
+)
+
+const (
+	ApplicationHost = "127.0.0.1:8080"
+	StartupTimeout  = 5
+)
+
+var (
+	ApplicationName    = findApplicationName()
+	ApplicationPIDFile = ApplicationName + ".pid"
+)
+
+func findApplicationName() string {
+	pwd, err := os.Getwd()
+	if err != nil {
+		abortLog(err)
+	}
+	return filepath.Base(pwd)
+}
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	killPrevious()
+
+	if err := build(); err != nil {
+		abortLog(err)
+	}
+
+	process, err := run()
+	if err != nil {
+		abortLog(err)
+	}
+	writePidFile(process.Pid)
+
+	if err := waitForReachable(StartupTimeout * time.Second); err != nil {
+		verboseLog("Application is unreachable after", StartupTimeout, "seconds")
+		kill(process)
+		abortLog("Cannot start tests")
+	}
+
+	verboseLog("Starting test suite")
+	suite := m.Run()
+
+	kill(process)
+
+	os.Exit(suite)
+}
+
+func build() error {
+	verboseLog("Building application", ApplicationName)
+
+	cmd := exec.Command("go", "build")
+
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout, cmd.Stderr = &stdout, &stderr
+
+	if err := cmd.Run(); err != nil {
+		err = fmt.Errorf("%v%v", stderr.String(), err)
+	}
+
+	return nil
+}
+
+func run() (*os.Process, error) {
+	verboseLog("Starting", ApplicationName, "as a child process")
+
+	cmd := exec.Command("./" + ApplicationName)
+
+	if testing.Verbose() {
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+	}
+
+	if err := cmd.Start(); err != nil {
+		log.Fatal(err)
+	}
+
+	verboseLog("Child process started with PID:", cmd.Process.Pid)
+	return cmd.Process, nil
+}
+
+func writePidFile(pid int) {
+	file, err := os.Create(ApplicationPIDFile)
+	defer file.Close()
+
+	if err != nil {
+		abortLog(err)
+	}
+
+	fmt.Fprint(file, pid)
+}
+
+func kill(p *os.Process) {
+	verboseLog("Killing child process")
+	if err := p.Kill(); err != nil {
+		traceLog("Could not kill", ApplicationName, "child is still running with PID:", p.Pid)
+	}
+
+	verboseLog("Removing test artifacts")
+	os.Remove(ApplicationPIDFile)
+	os.Remove(ApplicationName)
+}
+
+func killPrevious() {
+	content, err := ioutil.ReadFile(ApplicationPIDFile)
+	if err == nil {
+		verboseLog("Detected previous running instance with PID:", string(content))
+
+		pid, _ := strconv.Atoi(string(content))
+		p, _ := os.FindProcess(pid)
+		kill(p)
+	}
+}
+
+func waitForReachable(timeout time.Duration) error {
+	verboseLog("Waiting up to", StartupTimeout, "seconds for", ApplicationName, "to be reachable at", ApplicationHost)
+	if err := netutil.AwaitReachable(ApplicationHost, timeout); err != nil {
+		return err
+	}
+
+	verboseLog("Application Host is reachable, resuming")
+	return nil
+}
+
+func verboseLog(msg ...interface{}) {
+	if testing.Verbose() {
+		traceLog(msg...)
+	}
+}
+
+func traceLog(msg ...interface{}) {
+	fmt.Println(append([]interface{}{"-->"}, msg...)...)
+}
+
+func abortLog(err interface{}) {
+	traceLog(err)
+	os.Exit(1)
+}
+
+func TestItWorks(t *testing.T) {
+	res, err := goreq.Request{Uri: "http://" + ApplicationHost}.Do()
+	defer func() {
+		if res.Body != nil {
+			res.Body.Close()
+		}
+	}()
+
+	if err != nil {
+		t.Fatalf("Unexpected Request error: %s", err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Expected to receive %d but got %d instead", http.StatusOK, res.StatusCode)
+	}
+}
